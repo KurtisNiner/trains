34a31b73f81ff9efa425fe6821dd4e472fa1d240
Kurtis <kcarpe28@gmail.com>
2018-02-08 21:05:24 -0500

    go baby go

--- script.js
+++ script.js
@@ -6,28 +6,30 @@ var config = {
     projectId: "trains-5432c",
     storageBucket: "",
     messagingSenderId: "198411840912"
-  };
-  
+};
+//configures firebase
 firebase.initializeApp(config);
 
+//give a variable to the firebase to to refrence it
 var database = firebase.database();
 
+//give an array of variables that allow train times to be saved 
 var trains = [];
 
 //get user input when form is submitted to firebase
 
-$("#submit").on("click", function(){
-event.preventDefault();
+$("#submit").on("click", function () {
+    event.preventDefault();
 
-var trainNameInput = $("#name").val().trim();
-var destinationInput = $("#goingTo").val().trim();
-var firstArrivalInput = $("#first").val().trim();
-var frequencyInput = moment($("#frequency").val().trim()).format("X");
+    var trainNameInput = $("#name").val().trim();
+    var destinationInput = $("#goingTo").val().trim();
+    var firstArrivalInput = $("#first").val().trim();
+    var frequencyInput = moment($("#frequency").val().trim()).format("X");
 
-console.log(trainNameInput);
-console.log(destinationInput);
-console.log(firstArrivalInput);
-console.log(frequencyInput);
+    console.log(trainNameInput);
+    console.log(destinationInput);
+    console.log(firstArrivalInput);
+    console.log(frequencyInput);
 });
 
 
