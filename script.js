8eeb9635437bb1bab08b03df19785ef0d56e2b47
KurtisNiner <kcarpe28@gmail.com>
2018-02-19 22:28:49 -0500

    yes

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ firebase.initializeApp(config);
 //give a variable to the firebase in order to refrence it
 var database = firebase.database();
 // console.log(database);
+var now = moment();
 //give an array of variables that allow train times to be saved 
 // var timeNow = moment().format("hh:mm a");
 var trains = [];
@@ -36,24 +37,35 @@ $("#submit").on("click", function () {
     // console.log(firstArrivalInput);
     // console.log(frequencyInput);
 
+    //calculate time differences 
+    var firstArrival = moment(trainNameInput, "hh:mm a").subtract(1, "years");
+    var difference = moment().diff(moment(firstArrivalInput, "minutes"));
+    var timeRemaining = minutesUntilArrial % frequencyInput;
+    var timeUntilNextArrival = frequencyInput - timeRemaining;
+    var timeUntilNextTrain = moment().add(nextArrival, "minutes").format("hh:mm a");
+
     var trainSave = {
         trainName: trainNameInput,
         destination: destinationInput,
         firstArrive: firstArrivalInput,
-        frequency: frequencyInput
+        frequency: frequencyInput,
+        // nextTrainArrival: timeUntilNextArrival,
+        // waitTime: timeUntilNextTrain
     }
+
 
     database.ref().push(trainSave);
 });
 
 database.ref().on("child_added", function (childSnapshot, prevChildKey) {
-console.log(childSnapshot.val()); //database info
+    console.log(childSnapshot.val()); //database info
 
-var tr = $("<tr>");
-tr.text(childSnapshot.val().destination);
-$("#trainSchedule").append("<tr><td>" + childSnapshot.val().trainName + 
-"</td><td>" +  childSnapshot.val().destination + "</td><td>" + childSnapshot.val().firstArrive + "</td><td>"
-+ childSnapshot.val().frequency + "</td></tr>");
-
-
+    var tr = $("<tr>");
+    tr.text(childSnapshot.val().destination);
+    $("#trainSchedule").append("<tr><td>" + childSnapshot.val().trainName +
+        "</td><td>" + childSnapshot.val().destination + "</td><td>" + childSnapshot.val().firstArrive + "</td><td>"
+        + childSnapshot.val().frequency + "</td></tr>" );
+        // + nextTrainArrival + "</td><td>"
+        // + waitTime + "</td></tr>");
 })
+
